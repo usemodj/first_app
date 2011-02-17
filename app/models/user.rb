74d79ed267630ffee715a53039d07a3aaa815373
jinny <jinny@jinny-laptop>
2011-02-17 15:11:56 +0900

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ require 'digest'
 class User < ActiveRecord::Base
   has_many :microposts
   attr_accessor :password
-	attr_accessible  :name, :email, :password, :password_confirmation
+	attr_accessible  :name, :email, :password #, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
